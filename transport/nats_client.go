10e216b16f69412e062fd9779c47e5c50642567d
Loi Nguyen-Tan <ntloi@vnpt.vn>
2021-07-07 15:30:57 +0700

    nats reconnect

--- transport/nats_client.go
+++ transport/nats_client.go
@@ -1,8 +1,10 @@
 package transport
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
@@ -10,9 +12,12 @@ import (
 
 // ClientConstructor - ClientConstructor
 func ClientConstructor(url string) *nats.Conn {
+	// Initial
 	connectionID := uuid.New().String()
 	opts := []nats.Option{nats.Name("NATS_ExtensionLib_Requestor_" + connectionID)}
+	opts = SetupConnOptions(opts)
 
+	// Connect
 	nc, err := nats.Connect(url, opts...)
 	if err != nil {
 		log.Println("CONNECT TO NATS SERVER FAILED:", err)
@@ -25,4 +30,24 @@ func ClientConstructor(url string) *nats.Conn {
 // ClientDestructor - ClientDestructor
 func ClientDestructor(nc *nats.Conn) {
 	nc.Close()
+}
+
+// SetupConnOptions - SetupConnOptions
+func SetupConnOptions(opts []nats.Option) []nats.Option {
+	totalWait := 24 * 60 * 60
+	reconnectDelay := 2 * time.Second
+
+	opts = append(opts, nats.ReconnectWait(reconnectDelay))
+	opts = append(opts, nats.MaxReconnects(totalWait))
+	opts = append(opts, nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+		fmt.Println("Got disconnected!\nError Detail:", err)
+		fmt.Println("Reconnect attempt in", totalWait, "seconds")
+	}))
+	opts = append(opts, nats.ReconnectHandler(func(nc *nats.Conn) {
+		fmt.Println("Got reconnected to", nc.ConnectedUrl())
+	}))
+	opts = append(opts, nats.ClosedHandler(func(nc *nats.Conn) {
+		fmt.Println("Connection closed. Reason:", nc.LastError())
+	}))
+	return opts
 }
